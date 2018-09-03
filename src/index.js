5868f96ca3c4d6183ed40ee439b385bb1e321d55
kazuyaseki <track613armory@gmail.com>
2018-09-04 08:13:59 +0900

    Hello world

--- src/index.js
+++ src/index.js
@@ -4,5 +4,44 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import ApolloClient, { gql } from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${
+          process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+        }`
+      }
+    });
+  }
+});
+
+const query = gql`
+  {
+    organization(login: "apollographql") {
+      repositories(first: 5) {
+        nodes {
+          id
+          name
+          url
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`;
+
+client
+  .query({
+    query
+  })
+  .then(result => console.log(result));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
